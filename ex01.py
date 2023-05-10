dffece9319397a1c6723e374ab7f8dc9a917c321
BogdanChisu <bchisu@gmail.com>
2023-05-10 17:57:49 +0300

    revised code in main.py and replaced with code provided by trainer and added the 2nd solution and more efficient solution to Fibonacci

--- ex01.py
+++ ex01.py
@@ -26,3 +26,30 @@ nth = 30
 nth_element = fibonacci_recurent(nth)
 print(f"The {nth}th element of recursive Fibonacci equals = {nth_element}")
 print(f"Took = {perf_counter() - time_start}")
+
+@performance_measure
+def fibonacci_2nd_solution(n):
+    a = 0
+    b = 1
+
+    # Check is n is less
+    # than 0
+    if n < 0:
+        print("Incorrect input")
+
+    # Check is n is equal
+    # to 0
+    elif n == 0:
+        return 0
+
+    # Check if n is equal to 1
+    elif n == 1:
+        return b
+    else:
+        for i in range(1, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+print(fibonacci_2nd_solution(10000))
--- main.py
+++ main.py
@@ -1,106 +1,110 @@
-# modalitare de a adauga functionalitati unei functii
-"""
-activarea unei functii intr-un anume interval de timp
-date_wrapper
-"""
-# from time import sleep
-# from datetime import datetime
+from time import sleep
+from datetime import  datetime
 #
-# hour_now = datetime.now().hour
-# print(hour_now)
-#decorator pentru a rula functii doar la un anumit interval orar
+#
+# #decorator pentru a rula functii doar la un anumit interval orar
 # def decorator_interval(func):
 #     def wrapper():
-#
-#         if hour_now >= 7 and hour_now < 18:
+#         hour_now = datetime.now().hour
+#         if hour_now >= 7 and hour_now < 19:
 #             func()
 #         else:
-#             print("Outside specified interval")
+#             print("Outside time interval")
 #     return wrapper
 #
 # @decorator_interval
 # def fa_ceva():
-#     # sleep(2)
+#     sleep(2)
 #     print("Fac ceva")
 #
 # def fa_ceva_fara_decorator():
-#     # sleep(2)
-#     if hour_now >= 7 and hour_now < 22:
-#         print("fac_altceva")
+#     hour_now = datetime.now().hour
+#     if hour_now >= 7 and hour_now < 19:
+#         sleep(2)
+#         print("Fac ceva")
 #     else:
-#         print("Outside specified interval")
+#         print("Outside time interval")
+#
+# def fa_altceva_fara_decorator():
+#     hour_now = datetime.now().hour
+#     if hour_now >= 7 and hour_now < 21:
+#         sleep(2)
+#         print("Fac altceva")
+#     else:
+#         print("Outside time interval")
 #
 # fa_ceva()
-# fa_ceva_fara_decorator()
+# fa_altceva_fara_decorator()
 
-def decorator_with_args(func):
-
-    def wrapper(*args):
-        hour_now = datetime.now().hour
-        print(f"Cu 3 ore in urma a fost ora {int(hour_now) - 3}")
-        func(*args)
-        print(f"Peste 4 ore va fi ora {int(hour_now) + 4}")
-    return wrapper
-
-def my_function(v1, v2, v3):
-    print(f"Variabilele sunt {v1}, {v2}, {v3}")
-
-@decorator_with_args
-def my_function_2(v1, v2, v3):
-    print(f"Variabilele sunt {v1}, {v2}, {v3}")
+# def decorator_with_args(func):
+#     def wrapper(*args):
+#         hour_now = datetime.now().hour
+#         print(f"Cu 3 ore in urma a fost ora {int(hour_now)-3}")
+#         func(*args)
+#         print(f"Peste 4 ore va fi ora {int(hour_now)+4}")
+#     return wrapper
+# @decorator_with_args
+# def my_function(v1, v2, v3):
+#     print(f"Variabilele sunt {v1}, {v2}, {v3}")
 #
-# my_function_2(1, 2, 3)
-
-# def decorator_cu_etaj(var1, var2):
-#
+# def decorator_1etaj(var1, var2):
 #     def decorator_2etaj(func):
 #         def wrapper(*args):
-#             print(f"Variabilele 1e ale decoratorului sunt {var1} & {var2}")
+#             print(f"Variabilele 1e sunt {var1} & {var2}")
 #             return func(*args)
 #         return wrapper
-#     return decorator_2etaj()
+#     return decorator_2etaj
 #
-# @decorator_cu_etaj(45, 59)
-# def functia_mea(v1, v2, v3):
-#     print(f"Varibilele functie decorate sunt {v1} {v2} {v3}")
+# @decorator_1etaj(45,59)
+# def functia_mea(a,b,c):
+#     print(f" Variabile ale functiei decorate: {a},{b},{c}")
 #
+# #"Ion", "Elena", "Vasile"
 # functia_mea("Ion", "Elena", "Vasile")
-
+#
+# """
+# Rezultatul apelarii functiei decorate cu decorator ce contine variabile este:
+# Variabilele 1e sunt 45 & 59
+# Variabile ale functiei decorate: Ion,Elena,Vasile
+#
+# Adica s-au folosit variabilele decoratorului cat si variabilele functiei decorate
+# """
+#
 # class Studenti:
+#     denumire_scoala = "SDA"
 #     def __init__(self, varsta):
 #         self.varsta = varsta
 #
-#     # static method este un decorator prin care nu folosim deloc varibilele definite in clasa
-#     # un fel de 'helper function', independenta de clasa
+#     #staticmethod este un decorator prin care nu folosim deloc variabilele definite in clasa
+#     #un fel de helper function independenta de clasa, dar care ajuta in interiorul clasei
 #     @staticmethod
-#     def adult_sau_copil(self, cati_ani):
+#     def adult_sau_copil(cati_ani):
 #         if cati_ani >= 18:
 #             return "Adult"
 #         else:
 #             return "Copil"
 #
 #     @property
-#     def spune_ce_scoale(self):
+#     def spune_ce_scoala(self):
 #         return f"Scoala este {self.denumire_scoala}"
 
-
-# CHAINING DECORATORS
+#Chaining decorators - decoram cu mai multi decoratori o functie
 def primul_decorator(func):
-
     def wrapper(*args):
         print("Primul decorator")
         return func(*args)
     return wrapper
 
-def al_doilea_decorator(func):
+def doilea_decorator(func):
     def wrapper(*args):
         print("Al doilea decorator")
         return func(*args)
     return wrapper
-@primul_decorator
-@al_doilea_decorator
-def printare_Ceva(text_a):
-    print(f"Printez ceva {text_a}")
 
-printare_Ceva("Ce mai faci?")
+@primul_decorator
+@doilea_decorator
+def printare_ceva(text_a):
+    print(f"Printez ceva, anume = {text_a}")
+
+printare_ceva("Ce mai faci?")
 
